3907e553adaeac6581aafd77c22ed88aa8d852d9
oprty131 <slwmplylw@gmail.com>
2026-03-29 22:27:01 +0300

    Implement error handling for browser page creation
    Add error handling for browser initialization and launch.

--- main.py
+++ main.py
@@ -214,7 +214,24 @@ async def fakemessage(interaction: discord.Interaction, user: discord.User, text
     </body>
     </html>"""
 
+    try:
+      page = await browser.new_page()
+    except:
+     global playwright, browser
+     playwright = await async_playwright().start()
+     browser = await playwright.chromium.launch(
+          headless=True,
+          args=[
+              '--no-sandbox',
+              '--disable-setuid-sandbox',
+              '--disable-dev-shm-usage',
+              '--disable-gpu',
+              '--single-process',
+          ],
+          chromium_sandbox=False
+    )
     page = await browser.new_page()
+    
     await page.set_content(html_content)
 
     element = await page.query_selector('.container')
